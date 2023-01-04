ad5d76063c7a29e934d499a14d14ba1ee6f47cd3
TomariRyusei <t.ryu6635@gmail.com>
2023-01-04 15:59:03 +0900

    Complete notion clone app api

--- src/v1/controllers/memo.js
+++ src/v1/controllers/memo.js
@@ -8,7 +8,59 @@ exports.create = async (req, res) => {
       position: memoCount > 0 ? memoCount : 0,
     });
     res.status(201).json(memo);
-  } catch {
-    res.status(500).json(err);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+exports.getAll = async (req, res) => {
+  try {
+    const memos = await Memo.find({ user: req.user._id }).sort("-position");
+    res.status(200).json(memos);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+exports.getOne = async (req, res) => {
+  try {
+    const { memoId } = req.params;
+    const memo = await Memo.findOne({ user: req.user._id, _id: memoId });
+    if (!memo) return res.status(400).json("メモが存在しません");
+    res.status(200).json(memo);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+exports.update = async (req, res) => {
+  try {
+    const { memoId } = req.params;
+    const { title, description } = req.body;
+    if (title === "") req.body.title = "無題";
+    if (description === "")
+      req.body.description = "ここに自由に記入してください";
+    const currentMemo = await Memo.findById(memoId);
+    if (!currentMemo) return res.status(404).json("メモが存在しません");
+
+    const updatedMemo = await Memo.findByIdAndUpdate(memoId, {
+      $set: req.body,
+    });
+
+    res.status(200).json(updatedMemo);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+exports.delete = async (req, res) => {
+  try {
+    const { memoId } = req.params;
+    const memo = await Memo.findOne({ user: req.user._id, _id: memoId });
+    if (!memo) return res.status(400).json("メモが存在しません");
+    await Memo.deleteOne({ _id: memoId });
+    res.status(200).json("メモを削除しました");
+  } catch (error) {
+    res.status(500).json(error);
   }
 };
--- src/v1/routes/memo.js
+++ src/v1/routes/memo.js
@@ -3,5 +3,9 @@ const tokenHandler = require("../handlers/tokenHandler");
 const memoConttoller = require("../controllers/memo");
 
 router.post("/", tokenHandler.verifyToken, memoConttoller.create);
+router.get("/", tokenHandler.verifyToken, memoConttoller.getAll);
+router.get("/:memoId", tokenHandler.verifyToken, memoConttoller.getOne);
+router.put("/:memoId", tokenHandler.verifyToken, memoConttoller.update);
+router.delete("/:memoId", tokenHandler.verifyToken, memoConttoller.delete);
 
 module.exports = router;
